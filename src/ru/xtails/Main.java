d00f62fb336bf62757eb3f845acd70727c5b6f48
Iterus <supermax705@gmail.com>
2017-06-21 13:49:36 +0300

    Добавлен базовый интерфейс

--- src/ru/xtails/Main.java
+++ src/ru/xtails/Main.java
@@ -1,8 +1,15 @@
 package ru.xtails;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        JFrame frame = new JFrame("App");
+        frame.setContentPane(new MainWindow().mainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
---
+++ src/ru/xtails/MainWindow.java
@@ -0,0 +1,19 @@
+package ru.xtails;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainWindow {
+    JButton button1;
+    JPanel mainPanel;
+
+    public MainWindow() {
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "Hi");
+            }
+        });
+    }
+}
