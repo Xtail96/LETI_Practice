aeea20319ced9fa4a5bcd0c1c96f613820b21b8a
Xtail <xtail1996@yandex.ru>
2017-06-21 16:21:40 +0300

    Добавлен выбор входного файла из файловой системы и реализован вывод его содержимого

--- src/ru/xtails/MainWindow.java
+++ src/ru/xtails/MainWindow.java
@@ -1,8 +1,10 @@
 package ru.xtails;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.*;
 
 public class MainWindow {
     JButton openButton;
@@ -15,12 +17,42 @@ public class MainWindow {
     private JPanel inputTab;
     private JPanel displayTab;
     private JPanel resultTab;
+    private JPanel drawPanel;
 
     public MainWindow() {
         openButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(null, "Hi Bro");
+                //JOptionPane.showMessageDialog(null, "Hi Bro");
+                JFileChooser fileChooser = new JFileChooser();
+                FileNameExtensionFilter filter = new FileNameExtensionFilter("Text", "txt");
+                fileChooser.setFileFilter(filter);
+                int result = fileChooser.showOpenDialog(null);
+
+                if(result == JFileChooser.APPROVE_OPTION){
+                    String name = fileChooser.getSelectedFile().getName();
+                    String absolutePath = fileChooser.getSelectedFile().getAbsolutePath();
+
+                    File file = new File(absolutePath);
+                    BufferedReader reader = null;
+                    try {
+                        reader = new BufferedReader(new FileReader(file));
+                    } catch (FileNotFoundException e1) {
+                        e1.printStackTrace();
+                    }
+                    String content = "";
+                    String line;
+                    try {
+                        while((line = reader.readLine()) != null)
+                        {
+                            content += line;
+                            content += '\n';
+                        }
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    }
+                    inputTextArea.setText(content);
+                }
             }
         });
     }
