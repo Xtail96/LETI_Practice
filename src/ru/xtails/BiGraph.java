dd6feaa013b652dd2723609235473138b1403a8b
Iterus <supermax705@gmail.com>
2017-06-23 01:32:17 +0300

    Добавлены методы для добавления вершин в каждую долю. Метод addVertex теперь по-умолчанию добавляет вершину в 1 долю

--- src/ru/xtails/BiGraph.java
+++ src/ru/xtails/BiGraph.java
@@ -3,6 +3,7 @@ package ru.xtails;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.TreeSet;
 
 public class BiGraph extends Graph {
 
@@ -26,11 +27,104 @@ public class BiGraph extends Graph {
      */
     public BiGraph(Graph g) throws IllegalArgumentException {
         // создаем копию исходного графа
-        Graph h = new Graph(g);
+        super(g);
+
+        part1 = new HashMap<>();
+        part2 = new HashMap<>();
+
         // окрашиваем вершины
-        paintGraph(h);
+        paintGraph(this);
+
+        for (Vertex v : getVertices()) {
+            if (v.color % 2 == 0) {
+                // четные вершины добавляем в 1 долю
+                part1.put(v.name, v);
+            } else {
+                // нечетные во 2 долю
+                part2.put(v.name, v);
+            }
+
+        }
     }
 
+    /**
+     * Добавляет новую вершину по имени в 1-ю долю графа
+     * Вершина не добавляется в граф, если в графе уже есть вершина с таким же именем
+     * @param name имя вершины
+     * @return возвращает добавленную вершину, либо уже существующую в графе вершину
+     */
+    public Vertex addVertexToPart1(String name) {
+        Vertex v = new Vertex(name);
+        return addVertexToPart1(v);
+    }
+
+    /**
+     * Добавляет вершину в 1-ю долю графа
+     * Вершина не добавляется в граф, если в графе уже есть вершина с таким же именем
+     * @param v вершина
+     * @return возвращает добавленную вершину, либо уже существующую в графе вершину
+     */
+    public Vertex addVertexToPart1(Vertex v) {
+        Vertex w = vertices.get(v.name);
+
+        // если вершины нет в списке, добавляем
+        if (w == null) {
+            vertices.put(v.name, v);
+            adjList.put(v, new TreeSet<>());
+            part1.put(v.name, v);
+            V++;
+        }
+
+        return w;
+    }
+
+    /**
+     * Добавляет новую вершину по имени во 2-ю долю графа
+     * Вершина не добавляется в граф, если в графе уже есть вершина с таким же именем
+     * @param name имя вершины
+     * @return возвращает добавленную вершину, либо уже существующую в графе вершину
+     */
+    public Vertex addVertexToPart2(String name) {
+        Vertex v = new Vertex(name);
+        return addVertexToPart2(v);
+    }
+
+    /**
+     * Добавляет вершину во 2-ю долю графа
+     * Вершина не добавляется в граф, если в графе уже есть вершина с таким же именем
+     * @param v вершина
+     * @return возвращает добавленную вершину, либо уже существующую в графе вершину
+     */
+    public Vertex addVertexToPart2(Vertex v) {
+        Vertex w = vertices.get(v.name);
+
+        // если вершины нет в списке, добавляем
+        if (w == null) {
+            vertices.put(v.name, v);
+            adjList.put(v, new TreeSet<>());
+            part2.put(v.name, v);
+            V++;
+        }
+
+        return w;
+    }
+
+    /**
+     * Добавляет вершину в 1-ю долю графа
+     * Вершина не добавляется в граф, если в графе уже есть вершина с таким же именем
+     * @param v вершина
+     * @return возвращает добавленную вершину, либо уже существующую в графе вершину
+     */
+    @Override
+    public Vertex addVertex(Vertex v) {
+        return addVertexToPart1(v);
+    }
+
+    /**
+     * Раскрашивает заданный граф по правилу:
+     *      цвет вершины = цвет предыдущей вершины + 1
+     * @param g граф, который нужно раскрасить
+     */
     private void paintGraph(Graph g) {
         Vertex v;
         // запускаем bfs для всех непосещенных вершин
@@ -39,6 +133,11 @@ public class BiGraph extends Graph {
         }
     }
 
+    /**
+     * Выполняет обход графа в ширину и раскрашивает вершины
+     * @param g исходный граф
+     * @param root стартовая вершина
+     */
     private void bfs(Graph g, Vertex root) {
         Queue<Vertex> queue = new LinkedList<>();
         root.color = 0;
