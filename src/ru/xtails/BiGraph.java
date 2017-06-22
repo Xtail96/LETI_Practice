bcca494c9579ed2b1e16f17ede5650204cbb982a
Iterus <supermax705@gmail.com>
2017-06-22 23:22:17 +0300

    Добавлены конструктор двудольного графа по графу и копирующий конструктор графа

--- src/ru/xtails/BiGraph.java
+++ src/ru/xtails/BiGraph.java
@@ -9,8 +9,19 @@ public class BiGraph extends Graph {
      */
     private HashMap<String, Vertex> part1, part2;
 
+    /**
+     * Создает пустой двудольный граф
+     */
     public BiGraph() {
         part1 = new HashMap<>();
         part2 = new HashMap<>();
+    }
+
+    /**
+     * Создает двудольный граф по заданному графу
+     *
+     * @param g исходный граф
+     */
+    public BiGraph(Graph g) throws IllegalArgumentException {
     }
 }
