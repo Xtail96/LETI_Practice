9a2f776d6440eaa48bbb8b3859048587636a99ae
Iterus <supermax705@gmail.com>
2017-06-25 11:40:43 +0300

    Добавлена библиотека для отрисовки графов

--- src/ru/xtails/Visualizer.java
+++ src/ru/xtails/Visualizer.java
@@ -2,18 +2,35 @@ package ru.xtails;
 
 import javax.swing.*;
 import java.awt.*;
+import com.mxgraph.swing.mxGraphComponent;
+import com.mxgraph.view.mxGraph;
 
-/**
- * Created by Xtail on 24.06.17.
- */
-public class Visualizer extends JPanel{
+public class Visualizer extends JPanel {
+
+    public Visualizer() {
+        mxGraph graph = new mxGraph();
+        Object parent = graph.getDefaultParent();
+
+        graph.getModel().beginUpdate();
+
+            Object v1 = graph.insertVertex(parent, "A", null, 20, 20, 80, 30);
+            Object v2 = graph.insertVertex(parent, "B", null, 240, 150, 80, 30);
+            graph.insertEdge(parent, "E", null, v1, v2);
+
+        graph.getModel().endUpdate();
+
+        mxGraphComponent graphComponent = new mxGraphComponent(graph);
+        add(graphComponent);
+    }
+
     public void paintComponent(Graphics g){
         this.setBackground(Color.white);
+        /*
         g.setColor(Color.green);
         g.drawLine(10, 20, 100, 50);
         g.setColor(Color.blue);
         g.drawRect(25, 50, 100, 50);
         g.setColor(Color.gray);
-        g.fillOval(10, 90, 100, 50);
+        g.fillOval(10, 90, 100, 50);*/
     }
 }
