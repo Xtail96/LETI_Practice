5a69fed03d04d0e1d67cb40bb103d47c832a35f6
Iterus <supermax705@gmail.com>
2017-06-25 17:59:32 +0300

    Добавлены методы остановки и шага алгоритма; Добавлено описание методов

--- src/ru/xtails/MaximalMatchingKuhn.java
+++ src/ru/xtails/MaximalMatchingKuhn.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
  * Реализация алгоритма Куна для поиска наибольшего паросочетания в графе (в отдельном потоке)
  */
 public class MaximalMatchingKuhn implements Runnable {
+    private volatile boolean running = true;
     private volatile boolean paused = false;
     private final Object pauseLock = new Object();
     // исходный граф
@@ -17,14 +18,28 @@ public class MaximalMatchingKuhn implements Runnable {
         graph = g;
     }
 
+    /**
+     * Запуск потока
+     */
     @Override
     public void run() {
+        // для каждой вершины пытаемся найти увеличивающуюся цепь
         for (Vertex v : graph.getVertices()) {
-            graph.reset();
-            dfs(v);
+            if (running) {
+                graph.reset();
+                dfs(v);
+            } else {
+                // завершаемся
+                return;
+            }
         }
     }
 
+    /**
+     * Обход графа в глубину и улучшение текущего паросочетания
+     * @param root вершина, из которой будет запущен обход
+     * @return true, если удалось улучшеть текущее паросочетание, иначе false
+     */
     private boolean dfs(Vertex root) {
         if (root.visited)
             return false;
@@ -32,6 +47,7 @@ public class MaximalMatchingKuhn implements Runnable {
 
         for (Vertex to : graph.getNeighbours(root)) {
             if (!matching.containsKey(to) || dfs(matching.get(to))) {
+                // если удалось найти увеличивающуюся цепь, добавляем ребро в текущее паросочетание
                 matching.put(to, root);
                 return true;
             }
@@ -39,6 +55,9 @@ public class MaximalMatchingKuhn implements Runnable {
         return false;
     }
 
+    /**
+     * Приостанавливает выполнение алгоритма, если поле paused == true
+     */
     private void checkForPaused() {
         synchronized (pauseLock) {
             while (paused) {
@@ -51,14 +70,38 @@ public class MaximalMatchingKuhn implements Runnable {
         }
     }
 
+    /**
+     * Выставляет флаг приостановки алгоритма
+     */
     public void pause() {
         paused = true;
     }
 
+    /**
+     * Продолжает выполнение алгоритма
+     */
     public void resume() {
         synchronized (pauseLock) {
             paused = false;
-            pauseLock.notifyAll();
+            pauseLock.notify();
+        }
+    }
+
+    /**
+     * Выставляет флаг завершения алгоритма
+     */
+    public void stop() {
+        running = false;
+        resume();
+    }
+
+    /**
+     * Выполняет 1 шаг алгоритма
+     */
+    public void step() {
+        synchronized (pauseLock) {
+            paused = true;
+            pauseLock.notify();
         }
     }
 }
