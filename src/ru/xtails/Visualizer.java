f0b672f287266ad1865250e1441834e378960a0f
Xtail <xtail1996@yandex.ru>
2017-06-25 13:59:02 +0300

    Добавлен метод обновления графа; Осуществлен вывод двудольного графа на экран

--- src/ru/xtails/Visualizer.java
+++ src/ru/xtails/Visualizer.java
@@ -2,6 +2,7 @@ package ru.xtails;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.HashMap;
 
 import com.mxgraph.model.mxGeometry;
 import com.mxgraph.swing.mxGraphComponent;
@@ -13,42 +14,87 @@ public class Visualizer extends JPanel {
     final int leftPartX = 150;
     final int rightPartX = 600;
     final int vertexRadius = 50;
+    BiGraph g;
+    mxGraphComponent graphComponent;
 
+    /**
+     * Считывает граф
+     * @return считанный граф
+     */
+    public BiGraph readGraph() {
+        // добавить реализацию
+        g.addEdge("A", "1");
+        g.addEdge("B", "1");
+        g.addEdge("B", "2");
+        g.addEdge("C", "3");
+        g.addEdge("C", "4");
+        g.addEdge("D", "2");
+        g.addEdge("E", "1");
+        g.addEdge("E", "4");
+        g.addVertex("F");
+        return g;
+    }
 
     public Visualizer() {
+        g = new BiGraph();
+        g = readGraph();
+
         mxGraph graph = new mxGraph();
-        Object parent = graph.getDefaultParent();
-
-
-
-        graph.getModel().beginUpdate();
-
-            Object v1 = graph.insertVertex(parent, null, "A", leftPartX, 10, vertexRadius, vertexRadius, "shape=ellipse");
-            Object v2 = graph.insertVertex(parent, null, "B", rightPartX, 10, vertexRadius, vertexRadius, "shape=ellipse");
-            graph.insertEdge(parent, null, "", v1, v2);
-
-        graph.getModel().endUpdate();
-
 
         graph.setCellsSelectable(false);
         graph.setCellsEditable(false);
 
-        mxGraphComponent graphComponent = new mxGraphComponent(graph);
+        graphComponent = new mxGraphComponent(graph);
         graphComponent.setConnectable(false);
         graphComponent.setAutoScroll(true);
         graphComponent.setPreferredSize(new Dimension(800, 600));
 
         add(graphComponent);
+
+        update();
+    }
+
+    public void update(){
+        mxGraph graph = graphComponent.getGraph();
+        Object parent = graph.getDefaultParent();
+
+        graph.getModel().beginUpdate();
+
+
+        HashMap<Vertex, Object> graphToGui = new HashMap<>();
+
+        // рисуем 1 долю
+        int y = vertexRadius;
+        for(Vertex v1 : g.getPart1Vertices()) {
+            Object vertex = graph.insertVertex(parent, null, v1.name, leftPartX, y, vertexRadius, vertexRadius, "shape=ellipse");
+            /*for(Vertex v2 : g.getNeighbours(v1)) {
+                Object vertex2 = graph.insertVertex(parent, null, v2.name, rightPartX, y, vertexRadius, vertexRadius, "shape=ellipse");
+                graph.insertEdge(parent, null, "", vertex1, vertex2);
+            }*/
+            graphToGui.put(v1, vertex);
+            y += 2 * vertexRadius;
+        }
+
+        // рисуем 2 долю
+        y = vertexRadius;
+        for(Vertex v2 : g.getPart2Vertices()){
+            Object vertex = graph.insertVertex(parent, null, v2.name, rightPartX, y, vertexRadius, vertexRadius, "shape=ellipse");
+            graphToGui.put(v2, vertex);
+            y += 2 * vertexRadius;
+        }
+
+        // соединяем вершины ребрами
+        for(Vertex v1 : g.getPart1Vertices()){
+            for(Vertex v2 : g.getNeighbours(v1)){
+                Object vertex1 = graphToGui.get(v1), vertex2 = graphToGui.get(v2);
+                graph.insertEdge(parent, null, "", vertex1, vertex2, "endArrow=none;strokeColor=#00bb00");
+            }
+        }
+
+        graph.getModel().endUpdate();
     }
 
     public void paintComponent(Graphics g){
-        //this.setBackground(Color.white);
-        /*
-        g.setColor(Color.green);
-        g.drawLine(10, 20, 100, 50);
-        g.setColor(Color.blue);
-        g.drawRect(25, 50, 100, 50);
-        g.setColor(Color.gray);
-        g.fillOval(10, 90, 100, 50);*/
+        update();
     }
 }
