af57d6a3a6ebc937d49d93a118c9b40ee108061d
Iterus <supermax705@gmail.com>
2017-06-28 16:01:40 +0300

    Минимальный размер окна - 800x600

--- src/ru/xtails/MaximalMatching.java
+++ src/ru/xtails/MaximalMatching.java
@@ -1,12 +1,14 @@
 package ru.xtails;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MaximalMatching extends JFrame {
     public MaximalMatching() {
         setTitle("Наибольшее паросочетания в двудольном графе");
         setContentPane(new MainWindow().mainPanel);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setMinimumSize(new Dimension(800, 600));
         pack();
         setVisible(true);
     }
